b45768b1321048d323fe762319a21e2da928142b
Marcelo Boeira <me@marceloboeira.com>
2015-10-30 15:01:52 -0200

    feature: Add base `state machine` from the scratch

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
+#define SETUP_HANDLER 0
+
+short int CURRENT_STATE = SETUP_HANDLER;
+
+void setupHandler() {
+  // Hardware definitions
+  // Setup next state
+}
+
+void loop() {
+  while(1) {
+    switch(CURRENT_STATE) {
+      case SETUP_HANDLER: setupHandler(); break;
+    }
+  }
+}
+
+int main(void) {
+  loop();
   return 0;
 }
